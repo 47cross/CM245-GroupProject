59e9f38c0f5f6af364c01bc99184b4ed7adbe65d
Msmith9375790 <38225022+Msmith9375790@users.noreply.github.com>
2018-05-01 17:49:57 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,68 +1,74 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package CardGame;
-
-import java.util.ArrayList;
 
 /**
  *
- * @author matts
+ * @author Matt
  */
-public class Player {
+public class Player extends Deck {
     
-    private String name;    //Player name
-    private ArrayList<Card> hand = new ArrayList<>();   //Cards in the player's current hand
-    private int numCards;   //# of cards in the players hand
-    
+    private String name;
+    private ArrayList<Card> hand = new ArrayList<>();
+    private int numCards;
     
     public Player(String aName) {
         
         this.name = aName;
         this.emptyHand();
-    }
-    public void emptyHand() {}  //Reset the player's hand to have no cards
         
-    public int getHandSum() {   //Get the sum of the cards in the player's hand
+    }
+    public void emptyHand() {}
+    
+    public void addCard(Card c) {
+        hand.add(c);
+    }
+    
+    //Mutators
+    public String getName() {
+        return this.name;
+    }
+    public int getNumCards() {
+        return this.numCards;
+    }
+    
+    
+    public int getHandSum() {
         
         int handSum = 0;
         int cardNum;
         int numAces = 0;
-        // calculate each card's contribution to the hand sum
-        for(int c = 0; c < this.numCards; c++){
+        
+        for(int c = 0; c < this.numCards; c++) {
             
-            //get the number for the current card
-            cardNum = hand.getClass(c).getNumber();
+            cardNum = this.hand.get(c).getValue();
             
-            if(cardNum == 1) {  //Ace
+            if(cardNum == 1) {
                 numAces++;
-                handSum += 11;
-            } else if(cardNum > 10) {   //face card
-                handSum += 10;
+                handSum =+ 11;
+            } else if(cardNum > 10) {
+                handSum =- 10;
             } else {
-                handSum += cardNum;
+                handSum =+ cardNum;
             }
         }
-        //if we have aces and our sum is > 21, set some/all of them to value 1
-        //instead
+      
         while(handSum > 21 && numAces > 0) {
-            handSum -= 10;
+            handSum =- 10;
             numAces--;
         }
-        
         return handSum;
     }
-    public void printHand(boolean showFirstCard) {  //Print the cards in the player's hand
-        
-        System.out.printf("%s's cards: \n", this.name);
-        for(int c = 0; c < this.numCards; c++){
-            if(c == 0 && !showFirstCard){
-                System.out.println("   [hidden]");
-            } else {
-                System.out.printf("  %s\n", this.hand.getClass(c).toString());
+    public void printHand() {
+        for(int k = 0;k < hand.size();k++) {
+            System.out.println(hand.get(k));
+
             }
         }
     }
-}
+
