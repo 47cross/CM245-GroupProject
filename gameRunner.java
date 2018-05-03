f93093d311a6fb58a058b0a99b7cf42802111f1a
Msmith9375790 <38225022+Msmith9375790@users.noreply.github.com>
2018-05-03 10:18:38 -0500

    Update gameRunner.java

--- gameRunner.java
+++ gameRunner.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author Matt
+ * @author matts
  */
 public class gameRunner {
     public static void main(String[] args) {
@@ -17,18 +17,28 @@ public class gameRunner {
         Scanner in = new Scanner(System.in);
         Player D = new Player("Dealer");
         
-        System.out.print("Please enter your name: \n");
+        System.out.print("Please enter your name: ");
         Player P1 = new Player(in.next());
-        System.out.printf("Alright %s, are you ready to play? \n",P1.getName());
+        System.out.printf("\nAlright %s, are you ready to play? \n\n",P1.getName());
         
         Deck deck = new Deck();
         deck.createDeck();
         deck.shuffle();
         
+        D.addCard(deck.deal());
         P1.addCard(deck.deal());
+        D.addCard(deck.deal());
         P1.addCard(deck.deal());
+        D.printHand();
+        System.out.printf("%s's hand value: %d\n\n", D.getName(), D.getHandSum());
         P1.printHand();
-        System.out.println(P1.getHandSum());
+        System.out.printf("%s's hand value: %d\n\n", P1.getName(), P1.getHandSum());
+        
+        if(P1.getHandSum() == 21 && D.getHandSum() < 21)
+            System.out.printf("Congratulations %s! You have a blackjack!", P1.getName());
+        else
+            System.out.printf("Your card value is %d, would you like to hit or stay? (h/s)",P1.getHandSum());
+        
         
     }
 }
